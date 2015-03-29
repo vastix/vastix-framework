d94c3d73a3263c998e9571eedfd227dc26c82226
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2015-03-29 17:10:29 +0700

    Otw to BlazeGraph encapsulation - NanoSparqlServer and embedded

--- src/main/java/name/bpdp/kipo/verticles/blazegraph/BlazeGraph.java
+++ src/main/java/name/bpdp/kipo/verticles/blazegraph/BlazeGraph.java
@@ -2,16 +2,52 @@ package name.bpdp.kipo.verticles.blazegraph;
 
 import io.vertx.core.AbstractVerticle;
 
+import com.bigdata.journal.IIndexManager;
+import com.bigdata.rdf.sail.BigdataSail;
+
+import org.eclipse.jetty.server.Server;
+
 /*
  * @author <a href="http://bpdp.name">Bambang Purnomosidi</a>
  *
  */
 public class BlazeGraph extends AbstractVerticle {
 
-  @Override
-  public void start() throws Exception {
+	@Override
+	public void start() throws Exception {
 
-	  System.out.println("Hi from BlazeGraph verticle");
+		System.out.println("Starting BlazeGraph ... ");
+        final int port = 1234; // random port.
 
-  }
+        /*
+         * Create or re-open a durable database instance using default
+         * configuration properties. There are other constructors that allow you
+         * to take more control over this process.
+         */
+        final BigdataSail sail = new BigdataSail();
+
+        sail.initialize();
+
+        try {
+
+            final IIndexManager indexManager = sail.getDatabase()
+                    .getIndexManager();
+
+            final Map<String, String> initParams = new LinkedHashMap<String, String>();
+
+            SparqlEndpoint(port, indexManager, initParams).run();
+
+        } finally {
+
+            sail.shutDown();
+
+        }		
+
+	}
+
+	public void stop() {
+
+
+	}
+
 }
